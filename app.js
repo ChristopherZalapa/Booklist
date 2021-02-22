1606125170303c933146b2a59486846ff8f90fa6
ChristopherZalapa <chriszalapa@gmail.com>
2021-02-22 05:27:37 -0800

    Book Constructor

---
+++ app.js
@@ -0,0 +1,10 @@
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+
+// UI Constructor
+// Be Responisble for prototype methods add book delete book Errors
