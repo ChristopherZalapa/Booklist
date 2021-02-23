8f98a8242207c81d6c40407d995eaa3590393011
ChristopherZalapa <chriszalapa@gmail.com>
2021-02-23 12:37:10 -0800

    UI Methods

--- appes6.js
+++ appes6.js
@@ -7,5 +7,51 @@ class Book {
 }
 
 class UI {
+    addBookToList(book) {
+        // Create List
+        const list = document.getElementById('book-list');
+        // Create TR Element
+        const row = document.createElement('tr');
+        // Insert Cols
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.title}</td>
+        <td>${book.title}</td>
+        <td><a href="#" class="delete">X<a></td>
+        `;
 
+        list.appendChild(row);
+    }
+
+    showAlert(message, className) {
+        // Create Div
+        const div = document.createElement('div');
+        // Add Classes
+        div.className = `alert ${className}`;
+        // Add Text
+        div.appendChild(document.createTextNode(message));
+        // Get Parent
+        const container = document.querySelector('.container');
+        // Get Form
+        const form = document.querySelector('#book-form');
+        // Insert Alert
+        container.insertBefore(div, form);
+        // Timeout After 3 Seconds
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000)
+    }
+
+    deleteBook(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
 }
+
